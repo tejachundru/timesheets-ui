8d054e15c359e300553848339e7b0ca9b0e8127b
Teja Chundru <teja.chundru@netlinkz.com>
2024-10-05 17:30:01 +0530

    feat: add i18n support

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * A utility function that merges multiple class names into one.
+ */
 export function cn(...inputs: Array<ClassValue>): string {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Checks if the current environment is production.
+ *
+ * @returns {boolean} True if the current environment is production, false otherwise.
+ */
+export const isProduction = import.meta.env.MODE === "production";
---
+++ src/locales/i18n.ts
@@ -0,0 +1,49 @@
+import type { InitOptions } from "i18next";
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import type { HttpBackendOptions } from "i18next-http-backend";
+import Backend from "i18next-http-backend";
+
+import en from "./en/translations.json";
+import es from "./es/translations.json";
+import { isProduction } from "@/lib/utils";
+
+type TranslationKeys = typeof en; // Assuming `en` and `es` have the same structure
+
+const translations: Record<string, { translations: TranslationKeys }> = {
+  en: {
+    translations: en,
+  },
+  es: {
+    translations: es,
+  },
+};
+
+const config: InitOptions<HttpBackendOptions> = {
+  supportedLngs: ["en", "es"],
+  ns: ["translations"],
+  defaultNS: "translations",
+  fallbackLng: "en",
+  debug: !isProduction,
+  interpolation: {
+    escapeValue: false, // not needed for react as it escapes by default
+  },
+  resources: translations,
+  backend: {
+    loadPath: "/locales/{{lng}}/translations.json",
+  },
+};
+
+const i18n = i18next
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // detect user language
+  .use(LanguageDetector)
+  // backend plugin
+  .use(Backend)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init(config);
+
+export default i18n;
